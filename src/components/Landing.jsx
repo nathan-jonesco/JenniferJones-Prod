3f380a776352f0ef1a5cbf94ce08b17357152967
Nathan Jones <nathan@jonesco.autos>
2023-04-01 10:34:46 -0500

    h

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -55,11 +55,11 @@ export default function Example() {
           onClose={setMobileMenuOpen}
         >
           <div className="fixed inset-0 z-50" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-cyan-100 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-200 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <h1 className="h-8 w-auto font-cursive text-3xl text-cyan-600">
+                <h1 className="h-8 w-auto font-cursive text-3xl text-blue-600">
                   Jennifer Jones
                 </h1>
               </a>
@@ -74,12 +74,12 @@ export default function Example() {
             </div>
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
+                <div className="space-y-4 py-6">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-2xl font-semibold leading-7 tracking-wider text-cyan-900 hover:bg-gray-50"
+                      className="mt-5 block rounded-lg px-3 py-2 text-2xl font-semibold leading-7 tracking-wider text-blue-900 hover:bg-gray-50"
                     >
                       {item.name}
                     </a>
