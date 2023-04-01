9f7ecc8a8b8b120a64a9d292acefba85c900274c
Nathan Jones <nathan@jonesco.autos>
2023-04-01 09:21:40 -0500

    l

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,13 +13,13 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-cyan-200">
+    <div className="bg-gradient-to-r from-cyan-600 to-blue-600">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="bg-cyan-100 flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className=" flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <h1 className="text-3xl font-cursive h-8 w-auto">Jennifer Jones</h1>
+              <h1 className="text-3xl font-cursive h-8 w-auto text-cyan-50">👩🏼‍⚕️ Jennifer Jones</h1>
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -34,13 +34,13 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-md font-semibold leading-6 text-gray-900">
+              <a key={item.name} href={item.href} className="text-md font-semibold leading-6 text-cyan-100">
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="#" className="text-sm font-semibold leading-6 text-cyan-100">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -51,7 +51,7 @@ export default function Example() {
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <h1 className="text-3xl font-cursive h-8 w-auto">Jennifer Jones</h1>
+                <h1 className="text-3xl font-cursive h-8 w-auto text-cyan-600">Jennifer Jones</h1>
 
               </a>
               <button
@@ -97,7 +97,7 @@ export default function Example() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-cyan-100 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{' '}
               <a href="#" className="font-semibold text-cyan-600">
                 <span className="absolute inset-0" aria-hidden="true" />
@@ -106,20 +106,20 @@ export default function Example() {
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-3xl font-bold tracking-tight text-cyan-50 sm:text-6xl">
             Guiding Nurses to Their Dream Careers
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8 text-cyan-100">
             Meet Jennifer Jones, your dedicated nursing recruiter. Jennifer is a mother of three and is married to Johnathan Jones. Currently living in a camper in the beautiful state of Tennessee, Jennifer's mobile lifestyle allows her to work with nurses from all over the country.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+                className="rounded-md bg-cyan-100 px-3.5 py-2.5 text-sm font-semibold text-cyan-600 shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
               >
                 Get started
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a href="#" className="text-sm font-semibold leading-6 text-cyan-100">
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Jennifer Jones 👩🏼‍⚕️</title>
+        <title>👋🏼 Your Recruiter</title>
         <meta
           name="description"
           content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
